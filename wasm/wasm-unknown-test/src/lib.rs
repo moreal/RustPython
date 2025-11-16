98ca670e260a31e6785b6d7abb7860919b139ce6
Lee Dogeon <dev.moreal@gmail.com>
2025-11-16 21:56:29 +0900

    debug

--- wasm/wasm-unknown-test/src/lib.rs
+++ wasm/wasm-unknown-test/src/lib.rs
@@ -1,33 +1,14 @@
 use rustpython_vm::{AsObject, Context, Interpreter, convert::IntoObject, eval, types::TypeZoo};
 
-#[inline(never)]
-fn debug_fnaaaa(s: &str) -> usize {
-    s.len()
-}
-
 #[unsafe(no_mangle)]
 pub unsafe extern "C" fn eval(s: *const u8, l: usize) -> u32 {
     let src = std::slice::from_raw_parts(s, l);
     let Ok(src) = std::str::from_utf8(src) else {
         return 0;
     };
-    // return debug_assertion();
-    // let zoo = debug_init_type_hierarchy();
-    // let context = Context::genesis();
     let interpreter = Interpreter::without_stdlib(Default::default());
     return interpreter.enter(|vm| {
-        let res = match eval::eval(vm, src, vm.new_scope_with_builtins(), "<string>") {
-            Ok(res) => res,
-            Err(e) => {
-                if e.class().is(vm.ctx.exceptions.syntax_error) {
-                    let s = format!("{:?}", e.into_object().dict().unwrap());
-                    debug_fnaaaa(&s);
-                    return 4001;
-                } else {
-                    return 4000;
-                }
-            }
-        };
+        let res = eval::eval(vm, "2+10000", vm.new_scope_with_builtins(), "<string>").unwrap();
         res.try_into_value(vm).unwrap_or(3000)
     });
 }
